d0d1462b89614a7c7834427f512ab5d128da0e42
facuso162 <facu.sosa.gabriel@gmail.com>
2024-05-09 17:43:31 -0300

    update codigo ruleta

--- tp1.1/ruleta.py
+++ tp1.1/ruleta.py
@@ -5,132 +5,144 @@ from statistics import *
 from math import *
 from itertools import *
 
-# Crear una lista vacía para almacenar los resultados de las tiradas
-corridas_promedios = []
-media_esperada = 18
-frecuencia_esperada = 1 / 37
-varianza_esperada = 114
-desvio_esperado = 10.677
+# Datos
+CANT_NUMEROS = 37
+promedios_corridas = []
+f_absolutas_corridas = [0 for _ in range(CANT_NUMEROS)]
+resultados_totales = []
+MEDIA_ESPERADA = sum([x for x in range(CANT_NUMEROS)]) / CANT_NUMEROS
+FRECUENCIA_ESPERADA = 1 / CANT_NUMEROS
+VARIANZA_ESPERADA = sum([(x - MEDIA_ESPERADA)**2 for x in range(CANT_NUMEROS)]) / CANT_NUMEROS
+DESVIO_ESPERADO = sqrt(VARIANZA_ESPERADA)
 
-def promedio_por_tirada(cantidad_tiradas):
-    tiradas_promedios = []
-    resultados = []
-    for i in range(cantidad_tiradas): # for para las tiradas
-      # Generamos un número aleatorio entre 0 y 36
-      resultado_tirada = random.randint(0, 36)
-      resultados.append(resultado_tirada)
-      tiradas_promedios.append(mean(resultados)) # Se guarda el promedio luego de cada tirada
-    corridas_promedios.append(mean(resultados))
-    return tiradas_promedios
+# Funciones
+def promedio_por_tirada(resultados_tiradas):
+    promedios_tiradas = []
 
+    for i in range(1, cantidad_tiradas + 1):
+       # Se guarda el promedio luego de cada tirada
+       promedios_tiradas.append(mean(resultados_tiradas[:i]))
 
-def grafica_promedio(tiradas_promedios, cantidad_tiradas):
-  plt.plot(range(0,cantidad_tiradas),tiradas_promedios, color='red')
-  plt.axhline(y=media_esperada, color='b', label='Media Esperada')
+    return promedios_tiradas
+
+def frecuencia_relativa_por_tirada(resultados_tiradas):
+  f_relativas_tiradas = []
+
+  for i in range(1, cantidad_tiradas + 1): 
+      fr_num_elegido = resultados_tiradas[:i].count(numero_elegido) / len(resultados_tiradas[:i])
+      # Se guarda la frecuencia relativa luego de cada tirada
+      f_relativas_tiradas.append(fr_num_elegido)
+
+  return f_relativas_tiradas
+
+def frecuencia_absoluta_por_corrida(resultados_tiradas):
+  f_absolutas_tiradas = []
+
+  for num in range(CANT_NUMEROS):
+    # Se cuenta cuantas veces salio cada numero en las n tiradas de una corrida
+    f_absolutas_tiradas.append(resultados_tiradas.count(num))
+
+  return f_absolutas_tiradas
+
+def varianza_por_tirada(resultados_tiradas):
+  varianzas_tiradas = []
+
+  for i in range(1, cantidad_tiradas + 1): 
+    # Se guarda la varianza luego de cada tirada
+    varianzas_tiradas.append(pvariance(resultados_tiradas[:i]))
+
+  return varianzas_tiradas
+
+def desvio_stndr_por_tirada(varianzas_tiradas):
+  return [sqrt(var) for var in varianzas_tiradas]
+
+#Graficas
+def creacion_ventanas():
+  plt.figure('promedios_tiradas')
+  plt.figure('promedios_corridas')
+  plt.figure('f_relativas_tiradas')
+  plt.figure('f_absolutas_tiradas')
+  plt.figure('varianzas_tiradas')
+  plt.figure('desvios_standar_tiradas')
+
+def grafica_promedios_tiradas(promedios_tiradas, cantidad_tiradas):
+  plt.figure('promedios_tiradas')
+  plt.plot(range(cantidad_tiradas),promedios_tiradas,linewidth=0.2)
+  plt.axhline(y=MEDIA_ESPERADA,color='b',label='Media Esperada')
   plt.xlabel("Numero de tirada")
   plt.ylabel("Promedio")
   plt.title("Evaluacion del promedio sobre el conjunto de valores aleatorios")
 
-def grafica_promedios(corridas_promedios, cantidad_corridas):
-  plt.plot(range(0,cantidad_corridas),corridas_promedios, marker = 'o',color = 'r')
-  plt.axhline(y=media_esperada, color='b', label='Media Esperada')
+def grafica_promedios_corridas(promedios_corridas, cantidad_corridas):
+  plt.figure('promedios_corridas')
+  plt.plot(range(0,cantidad_corridas),promedios_corridas, marker = 'o',linewidth=0.7, markersize=3)
+  plt.axhline(y=MEDIA_ESPERADA, color='b', label='Media Esperada')
   plt.xlabel("Numero de corrida")
   plt.ylabel("Promedio")
-  plt.title("Evaluacion de la media aritmetica de cada corrida")
+  plt.title("Evaluacion del promedio de cada corrida")
   plt.grid()
 
-def frecuencia_relativa_por_tirada(cantidad_tiradas):
-  tiradas_frecuencia = []
-  resultados = []
-  for i in range(cantidad_tiradas): # for para las tiradas
-      # Generamos un número aleatorio entre 0 y 36
-      resultado_tirada = random.randint(0, 36)
-      resultados.append(resultado_tirada)
-      frecuencia_numero_elegido = resultados.count(numero_elegido) / len(resultados)
-      tiradas_frecuencia.append(frecuencia_numero_elegido)
-  return tiradas_frecuencia
-
-def grafica_frecuencia_relativa(tiradas_frecuencia, cantidad_tiradas):
-  plt.plot(range(0,cantidad_tiradas),tiradas_frecuencia)
-  plt.axhline(y=frecuencia_esperada, color='b', label='Frecuencia relativa esperada')
+def grafica_frecuencia_relativa(f_relativas_tiradas, cantidad_tiradas):
+  plt.figure('f_relativas_tiradas')
+  plt.plot(range(cantidad_tiradas),f_relativas_tiradas,color="red",linewidth=0.2)
+  plt.axhline(y=FRECUENCIA_ESPERADA,color='b',label='Frecuencia relativa esperada')
   plt.xlabel("Numero de tirada")
   plt.ylabel("Frecuencia relativa")
   plt.title("Evaluacion de la frecuencia relativa sobre el conjunto de valores aleatorios")
 
-def frecuencia_absoluta_por_corrida(cantidad_tiradas):
-  corridas_frecuencias = []
-  resultados = []
-  for i in range(cantidad_tiradas): # for para las tiradas
-      # Generamos un número aleatorio entre 0 y 36
-      resultado_tirada = random.randint(0, 36)
-      resultados.append(resultado_tirada)
-  for i in range(0,36):
-    corridas_frecuencias.append(resultados.count(i) / cantidad_tiradas)
-  return corridas_frecuencias
-
-def grafica_frecuencias_absolutas(corridas_frecuencias, cantidad_tiradas):
-  plt.plot(range(0,36),corridas_frecuencias)
-  plt.axhline(y=1/37, color = "gray")
+def grafica_frecuencias_absolutas(resultados_totales, cantidad_tiradas, cantidad_corridas):
+  plt.figure('f_absolutas_tiradas')
+  plt.hist(resultados_totales, CANT_NUMEROS, rwidth=0.75)
+  plt.axhline(y=cantidad_tiradas*cantidad_corridas/CANT_NUMEROS, color = "gray")
   plt.xlabel("Numeros obtenidos")
   plt.ylabel("Frecuencia absoluta")
   plt.title("Evaluacion de la frecuencia absoluta sobre el conjunto de valores aleatorios")
 
-
-def desvio_estandar_por_tiradas(cantidad_tiradas):
-  tiradas_desvio = []
-  resultados = []
-  for i in range(cantidad_tiradas): # for para las tiradas
-      # Generamos un número aleatorio entre 0 y 36
-      resultado_tirada = random.randint(0, 36)
-      resultados.append(resultado_tirada)
-      tiradas_desvio.append(pstdev(resultados))  #stdev calcula el desvio poblacional
-  return tiradas_desvio
-
-def grafica_desvio_estandar(tiradas_desvio, cantidad_tiradas):
-  plt.plot(range(0,cantidad_tiradas),tiradas_desvio)
-  plt.axhline(y=desvio_esperado, color='b', label='Desvío estandar esperado')
+def grafica_desvio_estandar(desvios_standar_tiradas, cantidad_tiradas):
+  plt.figure('desvios_standar_tiradas')
+  plt.plot(range(cantidad_tiradas),desvios_standar_tiradas,linewidth=0.2)
+  plt.axhline(y=DESVIO_ESPERADO, color='b', label='Desvío estandar esperado')
   plt.xlabel("Numero de tirada")
   plt.ylabel("Desvío estandar")
   plt.title("Evaluacion del desvío estandar sobre el conjunto de valores aleatorios")
 
-def varianza_por_tirada(cantidad_tiradas):
-  tiradas_varianza = []
-  resultados = []
-  for i in range(cantidad_tiradas): # for para las tiradas
-      # Generamos un número aleatorio entre 0 y 36
-      resultado_tirada = random.randint(0, 36)
-      resultados.append(resultado_tirada)
-      tiradas_varianza.append(pvariance(resultados))
-  return tiradas_varianza
-
-def grafica_varianza(tiradas_varianza, cantidad_tiradas):
-  plt.plot(range(0,cantidad_tiradas),tiradas_varianza)
-  plt.axhline(y=varianza_esperada, color='b', label='Varianza esperada')
+def grafica_varianza(varianzas_tiradas, cantidad_tiradas):
+  plt.figure('varianzas_tiradas')
+  plt.plot(range(0,cantidad_tiradas),varianzas_tiradas,linewidth=0.2)
+  plt.axhline(y=VARIANZA_ESPERADA, color='b', label='Varianza esperada')
   plt.xlabel("Numero de tirada")
   plt.ylabel("Varianza")
   plt.title("Evaluacion de la varianza sobre el conjunto de valores aleatorios")
 
-
+# Captura argumentos desde consola
 if len(sys.argv) != 7 or sys.argv[1] != "-n" or sys.argv[3] != "-c" or sys.argv[5] != "-e":
     print("Uso: python script.py -n <cantidad_tiradas> -c <corridas> -e <número_elegido>")
     sys.exit(1)
 cantidad_corridas = int(sys.argv[4])
 cantidad_tiradas = int(sys.argv[2])
 numero_elegido = int(sys.argv[6])
-for j in range(cantidad_corridas):
-  grafica_promedio(promedio_por_tirada(cantidad_tiradas), cantidad_tiradas)
-plt.show()
-grafica_promedios(corridas_promedios,cantidad_corridas)
-plt.show()
-for j in range(cantidad_corridas):
-  grafica_frecuencia_relativa(frecuencia_relativa_por_tirada(cantidad_tiradas), cantidad_tiradas)
-plt.show()
-for j in range(cantidad_corridas):
-  grafica_frecuencias_absolutas(frecuencia_absoluta_por_corrida(cantidad_tiradas), cantidad_tiradas)
-plt.show()
-for j in range(cantidad_corridas):
-  grafica_desvio_estandar(desvio_estandar_por_tiradas(cantidad_tiradas), cantidad_tiradas)
-plt.show()
-for j in range(cantidad_corridas):
-  grafica_varianza(varianza_por_tirada(cantidad_tiradas), cantidad_tiradas)
+
+# Creacion ventanas
+creacion_ventanas()
+
+# Ejecucion
+for _ in range(cantidad_corridas):
+  resultados_tiradas = [random.randint(0, CANT_NUMEROS - 1) for _ in range(cantidad_tiradas)]
+  resultados_totales.extend(resultados_tiradas) # Utilizado para la grafica de frecuencia absoluta
+  promedios_tiradas = promedio_por_tirada(resultados_tiradas)
+  promedios_corridas.append(mean(resultados_tiradas))
+  f_relativas_tiradas = frecuencia_relativa_por_tirada(resultados_tiradas)
+  f_absolutas_tiradas = frecuencia_absoluta_por_corrida(resultados_tiradas)
+  f_absolutas_corridas = [(n + f_absolutas_tiradas[i]) for (i, n) in enumerate(f_absolutas_corridas)]
+  varianzas_tiradas = varianza_por_tirada(resultados_tiradas)
+  desvios_standar_tiradas = desvio_stndr_por_tirada(varianzas_tiradas)
+
+  grafica_promedios_tiradas(promedios_tiradas, cantidad_tiradas)
+  grafica_frecuencia_relativa(f_relativas_tiradas, cantidad_tiradas)
+  grafica_varianza(varianzas_tiradas, cantidad_tiradas)
+  grafica_desvio_estandar(desvios_standar_tiradas, cantidad_tiradas)
+
+grafica_frecuencias_absolutas(resultados_totales, cantidad_tiradas, cantidad_corridas)
+grafica_promedios_corridas(promedios_corridas, cantidad_corridas)
+
 plt.show()
